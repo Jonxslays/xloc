fb8c7ebfca6d16a79bc38f964d4e4b5017607a04
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-11-05 16:17:01 -0700

    Add cli module and Parser struct

---
+++ src/cli.rs
@@ -0,0 +1,24 @@
+use clap::{App, Arg, ArgMatches};
+
+pub struct Parser<'a> {
+    pub matches: ArgMatches<'a>
+}
+
+impl<'a> Parser<'a> {
+    pub fn new() -> Self {
+        let cli = App::new("xloc")
+            .version("0.1.0")
+            .about("A fast, multi-threaded line counting utility.")
+            .arg(
+                Arg::with_name("jobs")
+                .short("j")
+                .long("jobs")
+                .value_name("NUM")
+                .help("The number of jobs (threads) to run")
+                .takes_value(true)
+                .default_value("1")
+            );
+
+        Self { matches: cli.get_matches() }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let parser = cli::Parser::new();
+
+    if let Some(jobs) = parser.matches.value_of("jobs") {
+        if let Ok(njobs) = jobs.parse::<usize>() {
+            println!("We would run {} jobs", njobs);
+        } else {
+            println!("We will run 1 job cause they passed bad input.");
+        }
+    }
 }
