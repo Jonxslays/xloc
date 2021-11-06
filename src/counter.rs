e1ddc4d171cb3124c77f1021f957d5959949091f
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-11-06 13:11:26 -0700

    Threading works

--- src/counter.rs
+++ src/counter.rs
@@ -3,27 +3,32 @@ use std::env;
 use std::path::PathBuf;
 use std::process;
 use std::io::Result;
-use std::sync::{Arc, Mutex};
+
 
 pub struct Counter {
     pub curdir: PathBuf,
-    pub files: Arc<Mutex<Vec<PathBuf>>>
+    pub files: Vec<PathBuf>
 }
 
 impl Counter {
-    pub fn new() -> Self {
-        let curdir = match env::current_dir() {
-            Ok(p) => p,
-            Err(e) => {
-                println!("{}", e);
-                process::exit(1);
-            },
-        };
+    pub fn new(dir: Option<PathBuf>) -> Self {
+        match dir {
+            Some(curdir) => return Self { curdir, files: vec![] },
+            None => {
+                let curdir = match env::current_dir() {
+                    Ok(p) => p,
+                    Err(e) => {
+                        println!("{}", e);
+                        process::exit(1);
+                    },
+                };
 
-        Self { curdir: curdir, files: Arc::new(Mutex::new(vec![]))}
+                return Self { curdir, files: vec![] };
+            }
+        }
     }
 
-    fn scan(&self, dir: &PathBuf) -> Result<()> {
+    fn scan(&mut self, dir: &PathBuf) -> Result<()> {
         if dir.is_dir() {
             for entry in fs::read_dir(dir)?
                 .filter(|d| {
@@ -41,7 +46,7 @@ impl Counter {
 
                 match path.is_dir() {
                     true => self.scan(&path)?,
-                    false => self.files.lock().unwrap().push(path),
+                    false => self.files.push(path),
                 }
             }
         }
@@ -49,19 +54,19 @@ impl Counter {
         Ok(())
     }
 
-    fn count_chunk(&self, size: usize, start: usize) -> Result<usize> {
-        let mut result = 0;
+    // fn count_chunk(&self, size: usize, start: usize) -> Result<usize> {
+    //     let mut result = 0;
 
-        for idx in start..(start + size) {
-            let content = fs::read_to_string(&self.files.lock().unwrap()[idx])?;
-            let split: Vec<&str> = content.split("\n").collect();
-            result += split.len();
-        }
+    //     for idx in start..(start + size) {
+    //         let content = fs::read_to_string(&self.files[idx])?;
+    //         let split: Vec<&str> = content.split("\n").collect();
+    //         result += split.len();
+    //     }
 
-        Ok(result)
-    }
+    //     Ok(result)
+    // }
 
-    fn generate_workloads(&self, njobs: usize, nfiles: usize) -> Result<Vec<usize>> {
+    pub fn generate_workloads(&self, njobs: usize, nfiles: usize) -> Result<Vec<usize>> {
         let chunk_size = nfiles / njobs;
         let remainder = nfiles % njobs;
 
@@ -74,24 +79,24 @@ impl Counter {
         Ok(workloads)
     }
 
-    pub fn count_files(&self) -> Result<usize> {
-        self.scan(&self.curdir)?;
-        Ok(self.files.lock().unwrap().len())
+    pub fn count_files(&mut self) -> Result<usize> {
+        self.scan(&self.curdir.clone())?;
+        Ok(self.files.len())
     }
 
-    pub fn count(&self, njobs: usize) -> Result<usize> {
-        let mut total = 0;
-        let nfiles = self.count_files()?;
-        let workloads = self.generate_workloads(njobs, nfiles)?;
-        let mut position = 0;
+    // pub fn count(&self, njobs: usize) -> Result<usize> {
+    //     let mut total = 0;
+    //     let nfiles = self.count_files()?;
+    //     let workloads = self.generate_workloads(njobs, nfiles)?;
+    //     let mut position = 0;
 
-        for load in workloads {
-            if let Ok(amount) = self.count_chunk(load, position) {
-                total += amount;
-                position += load;
-            }
-        }
+    //     for load in workloads {
+    //         if let Ok(amount) = self.count_chunk(load, position) {
+    //             total += amount;
+    //             position += load;
+    //         }
+    //     }
 
-        Ok(total)
-    }
+    //     Ok(total)
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,54 @@
-use std::process;
+use std::path::PathBuf;
+use std::{fs, thread};
+use std::sync::mpsc;
 
 mod cli;
 mod counter;
 
-// fn generate_thread_loads(njobs: usize, counter: &counter::Counter) -> Vec<usize> {
-//     let count = counter.count_files().unwrap();
-//     let initial = count / njobs;
-//     let remainder = count % njobs;
+fn handle_in_thread(tx: mpsc::Sender<usize>, files: Vec<PathBuf>) -> thread::JoinHandle<()> {
+    let handle = thread::spawn(move || {
+        let mut result = 0;
 
-//     let mut distribution = vec![initial; njobs];
+        for file in files {
+            let content = fs::read_to_string(file).unwrap();
+            let split: Vec<&str> = content.split("\n").collect();
+            result += split.len();
+        }
 
-//     for i in 0..remainder {
-//         distribution[i] += 1;
-//     }
+        tx.send(result).unwrap();
+    });
 
-//     distribution
-// }
+    handle
+}
 
 fn main() {
-    let counter = counter::Counter::new();
+    let mut counter = counter::Counter::new(None);
     let parser = cli::Parser::new();
+    let mut total: usize = 0;
+
+    let (tx, rx) = mpsc::channel();
 
     if let Some(jobs) = parser.matches.value_of("jobs") {
         let njobs = jobs.parse::<usize>().unwrap();
-        let total = match counter.count(njobs) {
-            Ok(t) => t,
-            Err(e) => {
-                println!("{}", e);
-                process::exit(1);
-            }
-        };
+        let nfiles = counter.count_files().unwrap();
+        let workloads = counter.generate_workloads(njobs, nfiles).unwrap();
+        let files = counter.files;
+        let mut position: usize = 0;
+        let mut handles: Vec<thread::JoinHandle<()>> = vec![];
 
-        println!("Total: {}", total);
+        for load in workloads {
+            handles.push(
+                handle_in_thread(
+                    tx.clone(), files[position..position + load].to_owned()
+                )
+            );
+            position += load;
+        }
+
+        for _ in handles {
+            total += rx.recv().unwrap();
+        }
+
+        println!("The total lines are: {}", total);
     }
 }
