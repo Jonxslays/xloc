b262adff8214b8d7cd21137a6818ff55d1cf18aa
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-11-13 18:21:38 -0700

    Add words flag to parser

--- src/parser.rs
+++ src/parser.rs
@@ -3,13 +3,14 @@ use clap::{App, Arg};
 pub struct Parser {
     pub njobs: usize,
     pub paths: Vec<String>,
+    pub words: bool
 }
 
 impl Parser {
     pub fn new() -> Self {
         let cli = App::new("xloc")
             .version("0.1.0")
-            .about("A fast, multi-threaded line counting utility.")
+            .about("A fast, multi-threaded line counting utility")
             .arg(
                 Arg::with_name("jobs")
                     .short("j")
@@ -18,6 +19,13 @@ impl Parser {
                     .help("The number of jobs (threads) to run")
                     .takes_value(true)
                     .default_value("1"),
+            )
+            .arg(
+                Arg::with_name("words")
+                    .short("w")
+                    .long("--words")
+                    .help("If included, count words instead of lines")
+                    .takes_value(false)
             )
             .arg(
                 Arg::with_name("path")
@@ -41,6 +49,11 @@ impl Parser {
             .parse::<usize>()
             .unwrap_or(1);
 
-        Self { njobs, paths }
+        let words = match matches.is_present("words") {
+            true => true,
+            false => false,
+        };
+
+        Self { njobs, paths, words }
     }
 }
