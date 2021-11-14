7604e9dee68fc0ad8291ee04634c02b1c6c40e06
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-11-14 14:09:53 -0700

    Add full testing suite
    Add dependencies for testing cli

---
+++ tests/app.rs
@@ -0,0 +1,105 @@
+use xloc;
+
+#[test]
+fn default_app() {
+    let app = xloc::App::default();
+    assert_eq!(app.get_njobs(), 1);
+    assert_eq!(app.get_words(), false);
+}
+
+#[test]
+fn new_app() {
+    let app = xloc::App::new(3, true);
+    assert_eq!(app.get_njobs(), 3);
+    assert_eq!(app.get_words(), true);
+}
+
+#[test]
+fn count_lines_dir() {
+    let app = xloc::App::default();
+    let base_path = String::from("tests/data");
+
+    let result = app.count(&base_path);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), 45);
+}
+
+#[test]
+fn count_words_dir() {
+    let app = xloc::App::new(1, true);
+    let base_path = String::from("tests/data");
+
+    let result = app.count(&base_path);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), 120);
+}
+
+#[test]
+fn count_lines_file() {
+    let app = xloc::App::default();
+    let base_path = String::from("tests/data");
+
+    // A vec containing tuples of file name and actual line count
+    let data_vec = vec![("/data.py", 17), ("/data.rs", 17), ("/data.txt", 11)];
+
+    // Iterate over data vec and make sure each files line counts match
+    for data in data_vec {
+        let mut path = base_path.clone();
+        path.push_str(data.0);
+
+        let result = app.count(&path);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), data.1);
+    }
+}
+
+#[test]
+fn count_words_file() {
+    let app = xloc::App::new(1, true);
+    let base_path = String::from("tests/data");
+
+    // A vec containing tuples of file name and actual word count
+    let data_vec = vec![("/data.py", 40), ("/data.rs", 36), ("/data.txt", 44)];
+
+    // Iterate over data vec and make sure each files word counts match
+    for data in data_vec {
+        let mut path = base_path.clone();
+        path.push_str(data.0);
+
+        let result = app.count(&path);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), data.1);
+    }
+}
+
+#[test]
+fn set_njobs() {
+    let mut app = xloc::App::default();
+    assert_eq!(app.get_njobs(), 1);
+    assert_eq!(app.set_njobs(3), 3);
+    assert_eq!(app.get_njobs(), 3);
+}
+
+#[test]
+fn set_words() {
+    let mut app = xloc::App::default();
+    assert_eq!(app.get_words(), false);
+    assert_eq!(app.set_words(true), true);
+    assert_eq!(app.get_words(), true);
+}
+
+#[test]
+fn derives() {
+    // Create 2 different apps
+    let app = xloc::App::default();
+    let app2 = xloc::App::new(1, true);
+
+    // Test Clone
+    let cloned = app.clone();
+
+    // Test Eq and Partialeq
+    assert_eq!(app, cloned);
+    assert!(app.eq(&cloned));
+    assert!(app.ne(&app2));
+    assert!(app2.ne(&cloned));
+}
